4bbc583968701750ee3af3b2eab0ed7b80c64ac6
Bastien Wermeille <bastien.wermeille@gmail.com>
2024-07-09 09:45:39 +0200

    Rename entry file

--- entry.cjs
+++
@@ -1,5 +0,0 @@
-// entry.cjs
-async function loadApp() {
-    const { app } = await import("./app.js"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
-}
-loadApp()
---
+++ entry.js
@@ -0,0 +1,4 @@
+async function loadApp() {
+    const { app } = await import("./app.js");
+}
+loadApp()
