c5fa032550a2718d9422a1d007266e7b6b0d6ffe
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-03-23 09:31:47 +0100

    Remove old cjs code

--- instrument.js
+++
@@ -1,8 +0,0 @@
-const Sentry = require("@sentry/node");
-require("dotenv").config();
-
-console.log(`GestSIS Print listening on port ${process.env.SENTRY_DSN}`);
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-});
